1f1dd220584f348e09f8bf5206184dc77e216ae7
Lies <Lies.deceuninck@gmail.com>
2015-10-22 17:52:13 +0200

    ordening mapjes

--- Data/Licht_verdeling/Traise/Traise_Code/Traise_plot.m
+++ Data/Licht_verdeling/Traise/Traise_Code/Traise_plot.m
@@ -65,19 +65,22 @@ k=1;
            %  subplot(1,2,1)                                 %Plot Fluence Rate (color) (mW/mm^)
            
              imagesc(r,z,dT(:,:,k)); c=colorbar;                  
-               % title({'Fluence Rate';['$\lambda=$' char(n_L) ', NA=' ...
-                %    char(n_NA) ', d=' char(n_d)]},'interpreter', 'LaTex');
-                %xlabel('r [mm]', 'interpreter', 'LaTex'); 
-                %ylabel('z [mm]', 'interpreter', 'LaTex');
-                %c.Label.String= '$\phi$ [$\frac{mW}{mm^2}$]';
-                %c.Label.FontSize=20;
-                %c.Label.Interpreter='latex';
+               title({'Temperature increase';['$\lambda=$' char(n_L) ', NA=' ...
+                   char(n_NA) ', d=' char(n_d)]},'interpreter', 'LaTex');
+                xlabel('r [mm]', 'interpreter', 'LaTex'); 
+                ylabel('z [mm]', 'interpreter', 'LaTex');
+                c.Label.String= '$dT$ [$K$]';
+                c.Label.FontSize=20;
+                c.Label.Interpreter='latex';
                 
-              % xlim([-0.1,0.1]); ylim([0,0.5]);
+              if j==1 
+                   xlim([-0.3,0.3]);ylim([0,0.6]);
+               elseif j==2 
+                   xlim([-1,1]);ylim([0,1.5]);   
+               end
             
                
-              % gfxfullfilename=fullfile(path,'MCML','MCML_FR_gfx',['MCML_gfx_FR' datafile_name]);
-            %saveas(fig, gfxfullfilename,'pdf');
+             
          k=k+1;
         
     end
